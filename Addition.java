fcae71674c3f3bc913fa72df9fd6684298fbc960
ArunAshokanSanthi <arunnandha3606@gmail.com.com>
2022-12-07 10:55:23 +0530

    new program

---
+++ Addition.java
@@ -0,0 +1,13 @@
+class Addition{
+	public static void main(String args[]){
+		Addition.addSum();
+	}
+	
+	public static int addSum(){
+		int a=10;
+		int b=5;
+		int sum=a+b;
+		System.out.println(sum);
+		return sum;
+	}
+}
